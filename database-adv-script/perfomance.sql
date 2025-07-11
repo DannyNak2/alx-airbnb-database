ebc46cd2a89bf07d3967d630b69b7e4d9140f25f
Daniel Nakachew Wudneh <danielnakachew1@gmail.com>
2025-07-11 16:21:15 +0300

    Update perfomance.sql
    update performance file

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,6 +1,10 @@
--- Initial unoptimized query
-SELECT b.*, u.name, p.name AS property_name, pay.amount
+-- Analyze performance of the query retrieving all bookings with user, property, and payment details
+
+EXPLAIN ANALYZE
+SELECT b.id AS booking_id, b.created_at, u.name AS user_name, p.name AS property_name, pay.amount
 FROM bookings b
 JOIN users u ON b.user_id = u.id
 JOIN properties p ON b.property_id = p.id
-JOIN payments pay ON b.id = pay.booking_id;
+JOIN payments pay ON b.id = pay.booking_id
+WHERE b.created_at IS NOT NULL
+AND pay.amount IS NOT NULL;
