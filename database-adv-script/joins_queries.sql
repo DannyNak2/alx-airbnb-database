32534dd31d5b21b7f398f7f4ae78ec2f2fd75d7e
Daniel Nakachew Wudneh <danielnakachew1@gmail.com>
2025-07-11 15:57:29 +0300

    Update joins_queries.sql
    edit joins queries file

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,9 +4,11 @@ FROM bookings b
 INNER JOIN users u ON b.user_id = u.id;
 
 -- LEFT JOIN: Properties and their Reviews (include properties with no reviews)
-SELECT p.*, r.review_text
+SELECT p.id AS property_id, p.name AS property_name, r.id AS review_id, r.review_text
 FROM properties p
-LEFT JOIN reviews r ON p.id = r.property_id;
+LEFT JOIN reviews r ON p.id = r.property_id
+ORDER BY p.id;
+
 
 -- FULL OUTER JOIN: All users and all bookings
 SELECT u.id AS user_id, u.name, b.id AS booking_id, b.property_id
